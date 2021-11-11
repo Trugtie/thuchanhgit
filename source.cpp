9ba507c4d6abd2a9aa37aa8b816d251ca4a0bb5b
Trugtie <nhamtrungtien2013@gmail.com>
2021-11-11 11:42:07 +0700

    Fix hieu

--- source.cpp
+++ source.cpp
@@ -3,13 +3,9 @@ using namespace std;
 int tong(int a, int b){
 	return a+b;
 };
-<<<<<<< HEAD
 int hieu(int a, int b){
-	return a-b;
+    return a-b;
 };
-=======
-int hieu(int a, int b);
->>>>>>> 0903cb1daa7b2592cb579879e6b00d53740caf89
 
 int main()
 {
@@ -21,12 +17,10 @@ int main()
 	cin >> b;
 	cout << "Chon phep toan (+,-) ";
 	cin >> phepToan;
-	if(phepToan=="+")
-<<<<<<< HEAD
-	cout << "Hieu= " + hieu(a,b);
-=======
-	count<<"Tong= "<<tong(a,b);
->>>>>>> 0903cb1daa7b2592cb579879e6b00d53740caf89
+	if(phepToan=='+')
+	cout<<"Tong= "<<tong(a,b);
+	else if(phepToan=='-')
+	cout<<"Hieu= "<<hieu(a,b);
 	system("pause");
 	return 0;
 }
